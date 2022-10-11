df1aaf1a560336af99691034bc0cb94d7ecda88b
Oleksiy Lyulin <oly@mita-teknik.com>
2022-10-11 21:07:14 +0300

    xAxis as datetime categories

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
+const ONE_HOUR = 3600000;
+const ONE_DAY = 24 * ONE_HOUR;
+
+const pointStart = Date.UTC(2020, 0, 1);
+const pointInterval = ONE_DAY;
+
+const data = Array.from(Array(365).keys()).map(() => 100 * Math.random());
+const categories = Array.from(Array(365).keys()).map(d =>
+  new Date(2020, 0, d).toDateString(),
+);
+
 const App = () => {
   const [chartOptions, setChartOptions] = useState<Highcharts.Options>({
+    chart: {
+      zooming: {
+        type: 'x',
+      },
+    },
+    plotOptions: {
+      column: {
+        borderWidth: 2,
+        borderColor: 'black',
+        opacity: 0.5,
+      },
+    },
+    xAxis: {
+      labels: {
+        rotation: undefined,
+        autoRotation: undefined,
+      },
+      crosshair: {
+        color: 'rgba(0, 0, 0, 0.1)',
+      },
+      type: 'datetime',
+      // minTickInterval: ONE_DAY,
+      categories,
+    },
     series: [
       {
-        type: 'line',
-        data: [1, 2, 3],
+        type: 'column',
+        // pointStart,
+        // pointInterval,
+        data,
       },
     ],
   });
-
-  useEffect(() => {
-    setInterval(() => {
-      setChartOptions({
-        series: [
-          {
-            type: 'line',
-            data: [Math.random() * 3, Math.random() * 3, Math.random() * 3],
-          },
-        ],
-      });
-    }, 1000);
-  }, []);
 
   return (
     <div>
